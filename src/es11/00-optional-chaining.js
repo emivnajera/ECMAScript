02661fc0fe9c3c8c9b2620539f20effd4406e709
emivnajera <emivelasquez2000@hotmail.com>
2022-12-05 09:24:38 -0600

    added matchall file.

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+  gndx: {
+    country: "GT",
+  },
+  ana: {
+    country: "CO",
+  },
+};
+
+console.log(users.gndx.country);
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 23987298372875367823n;
+const anotherBigNumber = BigInt(23987298372875367823);
+
+console.log(aBigNumber);
+console.log(anotherBigNumber);
